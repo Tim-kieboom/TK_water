7d71adecd907aae460d0018c910a8d1ba28a6109
tim kieboom <113353374+Tim-kieboom@users.noreply.github.com>
2024-08-05 21:41:45 +0200

    imbedded:L added  oscillate functionality in WaterPump class

--- TK_water_imbedded/src/waterPump/WaterPump.cpp
+++ TK_water_imbedded/src/waterPump/WaterPump.cpp
@@ -1,21 +1,26 @@
 #include "WaterPump.h"
 
 WaterPump::WaterPump(uint8_t pin)
+    : pin(pin)
 {
-    this->pin = pin;
     pinMode(pin, OUTPUT);
 }
 
-
-bool WaterPump::turnOnFor(uint16_t time, TimerType timeType)
+WaterPump::~WaterPump()
 {
-    static Timer* timer;
+    if(oscillateTimer != nullptr)
+        delete oscillateTimer;
+}
+
+bool WaterPump::turnOnFor(uint16_t time, TimerType timeType, bool turnOffBeforeTime/*default = false*/)
+{
+    static ITimer* timer;
     if(timer == nullptr)
         timer = new Timer(timeType);
 
     turnOn();
 
-    if(timer->waitTime(time))
+    if(timer->waitTime(time) || turnOffBeforeTime)
     {
         turnOff();
         
@@ -29,10 +34,55 @@ bool WaterPump::turnOnFor(uint16_t time, TimerType timeType)
 
 void WaterPump::turnOn()
 {
-    digitalWrite(pin, HIGH);
+    digitalWrite(pin, HIGH); 
+}
+
+bool WaterPump::oscillateFor(uint16_t time, TimerType timeType, uint16_t onTime, uint16_t offTime, bool turnOffBeforeTime/*default = false*/)
+{
+    static ITimer* timer;
+    if(timer == nullptr)
+        timer = new Timer(timeType);
+
+    oscillate(onTime, offTime);
+
+    if(timer->waitTime(time) || turnOffBeforeTime)
+    {
+        turnOff();
+        
+        timer = nullptr;
+        delete timer;
+        return true;
+    }
+
+    return false;
+}
+
+void WaterPump::oscillate(uint16_t onTime, uint16_t offTime)
+{
+    static bool isOn = false;
+
+    if(oscillateTimer == nullptr)
+        oscillateTimer = new Timer(millieSeconds);
+
+    uint16_t waitTime = (isOn) ? onTime : offTime;
+
+    if(oscillateTimer->waitTime(waitTime))
+        isOn = !isOn;
+
+
+    if(isOn)
+        digitalWrite(pin, HIGH);
+    else
+        digitalWrite(pin, LOW);
 }
 
 void WaterPump::turnOff()
 {
     digitalWrite(pin, LOW);
+
+    if(oscillateTimer != nullptr)
+    {
+        delete oscillateTimer;
+        oscillateTimer = nullptr;
+    }
 }
--- TK_water_imbedded/src/waterPump/WaterPump.h
+++ TK_water_imbedded/src/waterPump/WaterPump.h
@@ -9,11 +9,24 @@ class WaterPump
 {
 private:
     uint8_t pin;
+    ITimer* oscillateTimer = nullptr;
 
 public:
     WaterPump(uint8_t pin);
-    bool turnOnFor(uint16_t time, TimerType timeType);
+    ~WaterPump();
+
+    //Set turnOffBeforeTime to true if you need to interrupt and jump to something else (if you dont it will remember the timer and mess up the time)
+    bool turnOnFor(uint16_t time, TimerType timeType, bool turnOffBeforeTime = false);
     void turnOn();
+
+    /*
+    Set turnOffBeforeTime to true if you need to interrupt and jump to something else (if you dont it will remember the timer and mess up the time).
+    It will oscillate every 'dripFrequency' ms. 0 is no oscillation (only works in if it keep getting called in a loop).
+    */
+    bool oscillateFor(uint16_t time, TimerType timeType, uint16_t onTime, uint16_t offTime, bool turnOffBeforeTime = false);
+    //It will oscillate every 'dripFrequency' ms. 0 is no oscillation (only works in if it keep getting called in a loop)
+    void oscillate(uint16_t onTime, uint16_t offTime);
+
     void turnOff();
 };
 
